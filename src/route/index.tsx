31dd504e70b525cbe2e93c7b025da8e8e85cfbdb
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-03-04 15:28:55 +0330

    create products page

--- src/route/index.tsx
+++ src/route/index.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Register from "../components/Register";
+import Products from "../components/Products";
 
 export const AppRouter = () => (
   <Routes>
     <Route path="/" element={<Register />} />
+    <Route path="/products" element={<Products />} />
   </Routes>
 );
 
